315a6e4816af11b60cbbbd2c3d8b48a88a0af53f
Pablo Pulido Vallejo <pablopulidovl11@gmail.com>
2015-07-22 01:23:10 -0500

    Se removieron las carreras, dejando los departamentos

--- SistemaAdministrativo/Secretaria/url_secretaria.py
+++ SistemaAdministrativo/Secretaria/url_secretaria.py
@@ -5,8 +5,8 @@ from views_secretaria import *
 
 url_secretaria = [
 	url(r'^inicio-secretaria/$', inicio_secretaria),
-	url(r'^(computacion|electronica|biomedica|robotica|informatica)/listas/tCompleto$', listas_tCompleto),
-	url(r'^(computacion|electronica|biomedica|robotica|informatica)/listas/tMedio$', listas_tMedio),
-	url(r'^(computacion|electronica|biomedica|robotica|informatica)/form-incidencias$', form_incidencias),
-	url(r'^(computacion|electronica|biomedica|robotica|informatica)/reporte-incidencias$', incidencias),
+	url(r'^(computacion|electronica)/listas/tCompleto$', listas_tCompleto),
+	url(r'^(computacion|electronica)/listas/tMedio$', listas_tMedio),
+	url(r'^(computacion|electronica)/form-incidencias$', form_incidencias),
+	url(r'^(computacion|electronica)/reporte-incidencias$', incidencias),
 ]
