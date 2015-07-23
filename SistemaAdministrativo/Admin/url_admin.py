2e84197c0f9b3c7a3bd402a6ab84237e5d793e08
Alan Munguia <alanmunguiacerda@hotmail.com>
2015-07-23 12:16:16 -0500

    Modificada forma en que cargan los url del administrador para validación de permisos

--- SistemaAdministrativo/Admin/url_admin.py
+++ SistemaAdministrativo/Admin/url_admin.py
@@ -5,9 +5,10 @@ from apps.Usuarios.views import *
 from views_admin import *
 
 url_admin = [
-    url(r'^login/$', login),
+    url(r'^$', login),
     url(r'^logout/$', logout),
 	url(r'^inicio-administrador/$', inicio_admin),
-    url(r'^sistema/modificar/jefe-departamento', sistema_modificar_jefedep),
+    url(r'^sistema/modificar/jefe-departamento', form_sistema_modificar_jefedep),
+    url(r'^enviar/modificar/jefe-departamento', sistema_modificar_jefedep),
     url(r'^nuevo_departamento',nuevo_departamento),
 ]
--- SistemaAdministrativo/Admin/views_admin.py
+++ SistemaAdministrativo/Admin/views_admin.py
@@ -1,17 +1,40 @@
 # -*- encoding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 
-#Verificar si el usuario tiene permisos para estas vistas, por el momento sólo pide que esté logeado
-
-@login_required(login_url='/login/')
+#Verificar si el usuario está logeado, en caso contrario, redirecciona a página de log in
+@login_required(login_url='/')
 def inicio_admin(request):
-	return render(request, 'inicio-administrador.html', { 'banner' : True })
+    #revisa que el usuario tenga permisos necesarios para vier el contenido de esta página
+    if request.session['rol'] == 3:
+        banner = True
+        return render(request, 'inicio-administrador.html', locals())
+    else:
+        return render(request, 'PermisoDenegado.html')
 
-@login_required(login_url='/login/')
+@login_required(login_url='/')
+def form_sistema_modificar_jefedep(request):
+    if request.session['rol'] == 3:
+        if request.method == 'POST':
+            return render(request, 'modificar-jefe-departamento.html')
+        else:
+            return redirect('/inicio-administrador/')
+    else:
+        return render(request, 'PermisoDenegado.html')
+
+@login_required(login_url='/')
 def sistema_modificar_jefedep(request):
-	return render(request, 'modificar-jefe-departamento.html')
+    if request.session['rol'] == 3:
+        if request.method == 'POST':
+            #Aquí se modifica el jefe de departamento
+            return redirect('/inicio-administrador/')
+        return redirect('/inicio-administrador/')
+    else:
+        return render(request, 'PermisoDenegado.html')
 
-@login_required(login_url='/login/')
+@login_required(login_url='/')
 def nuevo_departamento(request):
-	return render(request, 'nuevo_departamento.html')
+    if request.session['rol'] == 3:
+        return render(request, 'nuevo_departamento.html')
+    else:
+        return render(request, 'PermisoDenegado.html')
