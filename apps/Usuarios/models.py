a32b30177dd84f6db45e7cf142210f9da4c15bfa
Alan Munguia <alanmunguiacerda@hotmail.com>
2015-07-23 23:55:25 -0500

    Agregado el campo de departamento a los usuarios (para los jefes de departamento, el campo puede ser null para secretarias)

--- apps/Usuarios/models.py
+++ apps/Usuarios/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from apps.Departamentos.models import Departamento
+
 # Create your models here.
 class Rol(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -15,6 +17,7 @@ class Usuario(models.Model):
     user = models.OneToOneField(User)
     codigo = models.CharField(max_length=9)
     rol = models.ForeignKey(Rol)
+    jefeDep = models.ForeignKey(Departamento, blank=True, null=True)
 
     def __unicode__(self):
         return self.user.username
