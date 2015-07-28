4aed2f92b8f94a7364c54426981e0bb3c16c472e
Drivit <drivit@Drivit-PC>
2015-07-28 00:02:42 -0500

    Archivos arreglados

---
+++ SistemaAdministrativo/url_secretaria.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from apps.Reportes.views import *
+
+url_secretaria = [
+	url(r'^inicio-secretaria/$', inicio_secretaria),
+	url(r'^(computacion|electronica)/listas/tCompleto$', listas_tCompleto),
+	url(r'^(computacion|electronica)/listas/tMedio$', listas_tMedio),
+	url(r'^(computacion|electronica)/form-incidencias$', form_incidencias),
+	url(r'^(computacion|electronica)/reporte-incidencias$', incidencias),
+]
