f7ddfa1329426d604356df1c7d33f57b9bc9b6b0
Alan Munguia <alanmunguiacerda@hotmail.com>
2015-08-04 14:19:27 -0500

    Agregado calendario jquery para compatibilidad

--- apps/Historicos/views.py
+++ apps/Historicos/views.py
@@ -32,8 +32,17 @@ def historicosFiltrados(request):
             fecha_expr = re.compile('[\d]{4}-[\d]{2}-[\d]{2}')
             res = fecha_expr.match(fecha)
             if res:
-                #fecha = fecha.split('-',3)
                 registros = Registro.objects.filter(fechaHoraModificacion__startswith=fecha).order_by("-fechaHoraModificacion")
+                paginator = Paginator(registros, 50)
+                pagina = request.GET.get('pagina')
+                try:
+                    registros = paginator.page(pagina)
+                except PageNotAnInteger:
+                    # If page is not an integer, deliver first page.
+                    registros = paginator.page(1)
+                except EmptyPage:
+                    # If page is out of range (e.g. 9999), deliver last page of results.
+                    registros = paginator.page(paginator.num_pages)
                 return render(request, 'historicos.html', locals())
             else:
                 registros = Registro.objects.all().order_by("-fechaHoraModificacion")
